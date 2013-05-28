484d3299d49c2a8d7fb2ff519ec3bc394b926bef
Ian Halpern <ian@ian-halpern.com>
2013-05-28 18:44:17 -0400

    Added setup.py for distutils

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='streetaddress',
+      version='0.1.0',
+      description='A Python port of the Perl address parser.',
+      author='Mike Jensen',
+      url='https://github.com/jjensenmike/python-streetaddress',
+      keywords='streetaddress',
+      packages=['streetaddress'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2',
+          'Topic :: Security',
+      ],
+     )
