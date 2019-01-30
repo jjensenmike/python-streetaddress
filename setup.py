298736608c93e6709dc13d05128b16f9436ecf38
Speaslee <arcadiapower@arcadiapowers-MacBook-Pro.local>
2019-01-30 15:48:04 -0700

    minor changes to work with python3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='streetaddress',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
-          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
           'Topic :: Security',
       ],
      )
